0be863ee734ea381170979f4184fb1994d714569
elenachekhina <elenachekhina@yandex.ru>
2023-08-16 22:27:37 +0400

    Mailer

--- app/mailers/auth_mailer.rb
+++ app/mailers/auth_mailer.rb
@@ -1,6 +1,4 @@
 class AuthMailer < ApplicationMailer
-  default from: 'notifications@example.com'
-
   def confirm_email
     @user = params[:user]
     mail(to: @user.email, subject: 'Email confirmation')
